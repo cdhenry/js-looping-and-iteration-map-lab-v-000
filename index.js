8557be0b16df58b26432f4e8ce2fb83ddd96b1cc
Chris Henry <cdhenry@gmail.com>
2018-04-21 21:31:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function lowerCaseDrivers(drivers){
 
 function nameToAttributes(drivers){
   debugger;
-  return drivers.map(function (driver) { return Object.assign({}, {firstName: driver.split(""), lastName: driver.split("")[1]}); });
+  return drivers.map(function (driver) { return Object.assign({}, {firstName: driver.split(" ")[0], lastName: driver.split(" ")[1]}); });
 }
 
 function attributesToPhrase(){
