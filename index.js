3de546d7d47936ddfad1d4b53ac8347708f998c8
Chris Henry <cdhenry@gmail.com>
2018-04-21 21:35:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ function nameToAttributes(drivers){
 }
 
 function attributesToPhrase(drivers){
-  return drivers.map(function (driver) { return console.log(`${driver.name} is from ${driver.hometown}`); });
+  return drivers.map(function (driver) { return `${driver.name} is from ${driver.hometown}`; });
 }
