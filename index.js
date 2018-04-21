71605e1ce3d0fc3284ac043cd2c2114fb60823e3
Chris Henry <cdhenry@gmail.com>
2018-04-21 21:16:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file.
 function lowerCaseDrivers(drivers){
-  return drivers.map(function(driver){return Object.assign({}, driver, driver.name); });
+  return drivers.map(function(driver){ return Object.assign({}, driver, driver.name.toLowerCase()); });
 }
 
 function nameToAttributes(){
