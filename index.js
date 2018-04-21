72d192808349be059fa6db6847bf2c9c0921375b
Chris Henry <cdhenry@gmail.com>
2018-04-21 21:33:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ function nameToAttributes(drivers){
 }
 
 function attributesToPhrase(drivers){
-  return drivers.map(function (driver) { return Object.assign({}, {firstName: driver.split(" ")[0], lastName: driver.split(" ")[1]}); });  
+  return drivers.map(function (driver) { return console.log(`${driver.name} is from ${driver.hometown}`)});
 }
