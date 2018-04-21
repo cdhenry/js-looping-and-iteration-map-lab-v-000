7b1f3a0dee9fe3d19e463f2517a2c081e0e816d3
Chris Henry <cdhenry@gmail.com>
2018-04-21 21:19:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file.
 function lowerCaseDrivers(drivers){
-  return drivers.map(drivers, function (driver) { return driver.name.toLowerCase(); });
+  return drivers.map(function (driver) { return driver.name.toLowerCase(); });
 }
 
 function nameToAttributes(){
